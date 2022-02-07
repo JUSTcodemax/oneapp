36409e58789300a9abbdfd422b95336a64fe7236
JUSTcodemax <978726425@qq.com>
2022-02-07 16:22:39 +0800

    0.0.1

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,18 +4,54 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName'), // 如需尝试获取用户信息可改为false
-    indicatorDots: true,
+    indicatorDots: false,
     autoplay: true,
     interval: 5000,
     duration: 500,
+    circular:true,
     imgLink: "../../images/0-1.jpg",
+    imgLink1: "../../images/0-2.jpg",
     Height: "50px",
+    previousMargin:"0px",
+    nextMargin:"0px",
+    imgpack:[
+      {link:"../../images/0-1.jpg"},
+      {link:"../../images/0-2.jpg"},
+      {link:"../../images/0-3.jpg"},
+      {link:"../../images/0-4.jpg"},
+      {link:"../../images/0-5.jpg"},
+      {link:"../../images/0-6.jpg"},
+      {link:"../../images/0-7.jpg"},
+      {link:"../../images/0-8.jpg"},
+      {link:"../../images/0-9.jpg"},
+      {link:"../../images/1-1.jpg"},
+      {link:"../../images/1-2.jpg"},
+      {link:"../../images/1-3.jpg"},
+      {link:"../../images/1-4.jpg"},
+      {link:"../../images/1-5.jpg"},
+      {link:"../../images/1-6.jpg"},
+      {link:"../../images/1-7.jpg"},
+      {link:"../../images/1-8.jpg"},
+      {link:"../../images/1-9.jpg"},
+      {link:"../../images/2-1.jpg"},
+      {link:"../../images/2-2.jpg"},
+      {link:"../../images/2-3.jpg"},
+      {link:"../../images/2-4.jpg"},
+      {link:"../../images/2-5.jpg"},
+      {link:"../../images/2-6.jpg"},
+      {link:"../../images/2-7.jpg"},
+      {link:"../../images/2-8.jpg"},
+      {link:"../../images/2-9.jpg"},
+      {link:"../../images/3-1.jpg"},
+      {link:"../../images/3-2.jpg"},
+      {link:"../../images/3-3.jpg"},
+      {link:"../../images/3-4.jpg"},
+      {link:"../../images/3-5.jpg"},
+      {link:"../../images/3-6.jpg"},
+      {link:"../../images/3-7.jpg"},
+      {link:"../../images/3-8.jpg"},
+      {link:"../../images/3-9.jpg"},
+    ]
   },
   ChangeHeightAndpage: function (e) {
     var ScreenWidth = wx.getSystemInfoSync().windowWidth;
@@ -25,13 +61,27 @@ Page({
     this.setData({
       Height: height
     })
-    this.sleep(5000);
-    var x = 0,
+    /*
+    var x = 1,
       y = 1;
-    while (0 <= x <= 3 && 1 <= y <= 9) {
+    while (1 <= x <= 2 && 1 <= y <= 9) {
       var text = "../../images/" + x + "-" + y + ".jpg";
       this.setData({
         imgLink: text
+      })
+      if (y == 9) {
+        if (x == 2) {
+          x = 1;
+        } else {
+          x++;
+        }
+        y = 1;
+      } else {
+        y++;
+      }
+      var text = "../../images/" + x + "-" + y + ".jpg";
+      this.setData({
+        imgLink1: text
       })
       var ScreenWidth = wx.getSystemInfoSync().windowWidth;
       var imgWidth = e.detail.width;
@@ -40,18 +90,8 @@ Page({
       this.setData({
         Height: height
       })
-      this.sleep(5000);
-    }
-    if (y == 9) {
-      if (x == 3) {
-        x = 0;
-      } else {
-        x++;
-      }
-      y = 1;
-    } else {
-      y++;
-    }
+      this.sleep(1000);
+    }*/
   },
   sleep(Millis) {
     var now = new Date();
@@ -63,39 +103,4 @@ Page({
       }
     }
   },
-
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
-  },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    })
-  },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })
